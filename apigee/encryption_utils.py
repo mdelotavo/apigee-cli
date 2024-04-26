814e63e491bee67e253e8c0dec61ad776e9a67a9
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2024-04-26 17:38:59 +1000

    Clean up code

--- apigee/encryption_utils.py
+++ apigee/encryption_utils.py
@@ -1,5 +1,4 @@
 import base64
-
 import gnupg
 
 ENCRYPTED_HEADER_BEGIN = "-----BEGIN ENCRYPTED APIGEE CLI MESSAGE-----"
@@ -8,24 +7,20 @@ ENCRYPTED_HEADER_END = "-----END ENCRYPTED APIGEE CLI MESSAGE-----"
 
 def encrypt_message_with_gpg(secret, message, encoded=True):
     gpg = gnupg.GPG()
-    if encoded:
-        return base64.b64encode(
-            str(
-                gpg.encrypt(
-                    message, symmetric="AES256", passphrase=secret, recipients=None
-                )
-            ).encode()
-        ).decode()
-    return str(
-        gpg.encrypt(message, symmetric="AES256", passphrase=secret, recipients=None)
+    encrypted_message = gpg.encrypt(
+        message, symmetric="AES256", passphrase=secret, recipients=None
     )
+    if encoded:
+        return base64.b64encode(str(encrypted_message).encode()).decode()
+    return str(encrypted_message)
 
 
 def has_encrypted_header(message):
     if not isinstance(message, str):
         return False
-    return message.startswith(ENCRYPTED_HEADER_BEGIN) and message.endswith(
-        ENCRYPTED_HEADER_END
+    return (
+        message.startswith(ENCRYPTED_HEADER_BEGIN)
+        and message.endswith(ENCRYPTED_HEADER_END)
     )
 
 
@@ -35,5 +30,6 @@ def decrypt_message_with_gpg(secret, message, encoded=True):
         return ""
     message = message[len(ENCRYPTED_HEADER_BEGIN) : -len(ENCRYPTED_HEADER_END)]
     if encoded:
-        return str(gpg.decrypt(base64.b64decode(message).decode(), passphrase=secret))
+        decoded_message = base64.b64decode(message).decode()
+        return str(gpg.decrypt(decoded_message, passphrase=secret))
     return str(gpg.decrypt(message, passphrase=secret))
