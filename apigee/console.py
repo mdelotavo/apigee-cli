dbdbcbcce234ce0d215ee9528ba39821be6d0ddc
Matthew.Delotavo <matthew.delotavo@service.nsw.gov.au>
2026-06-26 14:02:31 +1000

    refactor

--- apigee/console.py
+++ apigee/console.py
@@ -2,14 +2,24 @@ import builtins
 import sys
 
 
-def echo(*message, exit_status=None, make_silent=False, current_verbosity=0, expected_verbosity=0, line_ending="\n", should_flush=False):
-    toggle_silent = builtins.APIGEE_CLI_TOGGLE_SILENT
-    toggle_verbose = builtins.APIGEE_CLI_TOGGLE_VERBOSE
-    if make_silent or toggle_silent:
-        if exit_status:
+def echo(
+  *message,
+  exit_status=None,
+  make_silent=False,
+  current_verbosity=0,
+  expected_verbosity=0,
+  line_ending="\n",
+  should_flush=False,
+):
+    if make_silent or builtins.APIGEE_CLI_TOGGLE_SILENT:
+        if exit_status is not None:
             sys.exit(exit_status)
         return
-    if max(current_verbosity, toggle_verbose) >= expected_verbosity:
+
+    verbosity = max(current_verbosity, builtins.APIGEE_CLI_TOGGLE_VERBOSE)
+
+    if verbosity >= expected_verbosity:
         print(*message, end=line_ending, flush=should_flush)
-    if exit_status:
+
+    if exit_status is not None:
         sys.exit(exit_status)
