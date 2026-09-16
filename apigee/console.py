90bfba51ccef5a6aa5cf66e0cda60edf665ef06e
Matthew.Delotavo <matthew.delotavo@service.nsw.gov.au>
2026-09-16 20:14:00 +1000

    fix

--- apigee/console.py
+++ apigee/console.py
@@ -1,11 +1,12 @@
 import builtins
 import sys
 from dataclasses import dataclass, fields, replace
+from typing import Optional
 
 
-@dataclass(slots=True)
+@dataclass
 class EchoOptions:
-    exit_status: int | None = None
+    exit_status: Optional[int] = None
     silent: bool = False
     verbosity: int = 0
     level: int = 0
@@ -13,14 +14,18 @@ class EchoOptions:
     flush: bool = False
 
 
-def echo(*msg, options: EchoOptions | None = None, **kwargs):
+def echo(*msg, options=None, **kwargs):
+    # type: (*object, Optional[EchoOptions], **object) -> None
+
     options = options or EchoOptions()
 
     valid_fields = {f.name for f in fields(EchoOptions)}
 
     for key, value in kwargs.items():
         if key not in valid_fields:
-            raise TypeError(f"echo() got an unexpected keyword argument '{key}'")
+            raise TypeError(
+                "echo() got an unexpected keyword argument '{}'".format(key)
+            )
         options = replace(options, **{key: value})
 
     if options.silent or builtins.APIGEE_CLI_TOGGLE_SILENT:
@@ -28,7 +33,10 @@ def echo(*msg, options: EchoOptions | None = None, **kwargs):
             sys.exit(options.exit_status)
         return
 
-    verbosity = max(options.verbosity, builtins.APIGEE_CLI_TOGGLE_VERBOSE)
+    verbosity = max(
+        options.verbosity,
+        builtins.APIGEE_CLI_TOGGLE_VERBOSE,
+    )
 
     if verbosity >= options.level:
         print(*msg, end=options.end, flush=options.flush)
